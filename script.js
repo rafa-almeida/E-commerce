07102872fa1fd71d04a003f8876cd53662fa2944
rafa-almeida <rafasegt@gmail.com>
2023-06-04 23:40:29 -0300

    adicionando menu hamburguer

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ function trocar(){
     imgAtual = imgAnterior2;
     imgAnterior2 = aux;
 }
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
